e45e304a39071b3442a2e504d46f52a3f21a81b6
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-12 16:23:40 +0100

    updated

--- hello/src/main/java/com/jai/springboot/hello/App.java
+++ hello/src/main/java/com/jai/springboot/hello/App.java
@@ -1,9 +1,13 @@
 package com.jai.springboot.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -11,16 +15,56 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  */
 @Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
 
 	public static void main(String args[]) {
 		SpringApplication.run(App.class, args);
 	}
 
-	@RequestMapping(path = "/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	@ResponseBody
-	String helloApi() {
-		return "Hello";
+	Person helloApi() {
+		return new Person("Tom", 23);
 	}
+
+	@RequestMapping(path = "/all", method = RequestMethod.GET, produces = { "application/json" })
+	@ResponseBody
+	List<Person> getPersons() {
+		List<Person> persons = new ArrayList<Person>();
+		persons.add(new Person("Harry", 11));
+		persons.add(new Person("Mickey", 121));
+		persons.add(new Person("Ravi", 21));
+		return persons;
+	}
+
+}
+
+class Person {
+
+	String name;
+	int age;
+
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
