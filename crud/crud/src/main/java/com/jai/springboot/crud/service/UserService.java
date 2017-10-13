f80237b8b8cba754e72b0d0ba7a00ef6d8af43e3
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-13 18:00:28 +0100

    crud added

---
+++ crud/crud/src/main/java/com/jai/springboot/crud/service/UserService.java
@@ -0,0 +1,42 @@
+package com.jai.springboot.crud.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jai.springboot.crud.dao.UserDao;
+import com.jai.springboot.crud.entity.User;
+
+/**
+ * Service for user
+ * 
+ * @author jjayaraman
+ *
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	UserDao userDao;
+
+	/**
+	 * Get users
+	 * 
+	 * @return
+	 */
+	public Map<Integer, User> getUsers() {
+		return userDao.getUsers();
+	}
+
+	/**
+	 * Get a user by id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public User getUser(int id) {
+		return userDao.getUser(id);
+	}
+
+}
