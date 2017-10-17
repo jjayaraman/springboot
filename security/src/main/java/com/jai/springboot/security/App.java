9659227333e4525fbe3358f443bf66ad45dca2fd
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-17 18:29:04 +0100

    added

--- security/src/main/java/com/jai/springboot/security/App.java
+++ security/src/main/java/com/jai/springboot/security/App.java
@@ -1,13 +1,17 @@
 package com.jai.springboot.security;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
