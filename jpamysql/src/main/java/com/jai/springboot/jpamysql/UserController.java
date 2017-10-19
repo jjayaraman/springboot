587a68875fbe1ebf5daa0bd31cb78c786817a548
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-19 16:51:37 +0100

    for JPA msql - working

--- jpamysql/src/main/java/com/jai/springboot/jpamysql/UserController.java
+++ jpamysql/src/main/java/com/jai/springboot/jpamysql/UserController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +21,15 @@ public class UserController {
 	public List<User> getUsers() {
 		return userRepository.findAll();
 	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void create(@RequestBody User user) {
+		userRepository.save(user);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+	public void delete(@PathVariable long id) {
+		userRepository.delete(id);
+	}
+
 }
--- jpamysql/src/main/java/com/jai/springboot/jpamysql/UserRepository.java
+++ jpamysql/src/main/java/com/jai/springboot/jpamysql/UserRepository.java
@@ -2,6 +2,6 @@ package com.jai.springboot.jpamysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
