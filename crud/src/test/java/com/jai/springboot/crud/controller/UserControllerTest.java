d5d0f3257badeb9e95ea1e1cd30a7776812f3f4a
Jayakumar Jayaraman <india.jai@gmail.com>
2019-02-03 08:11:30 +0000

    test added

--- crud/src/test/java/com/jai/springboot/crud/controller/UserControllerTest.java
+++ crud/src/test/java/com/jai/springboot/crud/controller/UserControllerTest.java
@@ -1,7 +1,5 @@
 package com.jai.springboot.crud.controller;
 
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,33 +11,47 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+//@formatter:off
 public class UserControllerTest {
 
 	private final String API_BASE = "http://localhost:8080";
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
-	}
-
-	@Test
-	public void test1() {
+	public void testGetAllUsers() {
 		Response res = get(API_BASE + "/users");
 
 		Assert.assertEquals(res.getStatusCode(), 200);
 		Assert.assertEquals(res.getContentType(), "application/json;charset=UTF-8");
 
-		
-		when().
-			get(API_BASE + "/users").
-		then().
-			statusCode(200).
-		and().
-			contentType(ContentType.JSON);
+		when().get(API_BASE + "/users").then().statusCode(200).contentType(ContentType.JSON);
 
-		List<User> user = res.getBody().jsonPath().getList("");
-		System.out.println("xxx ::  " + user);
+//		List<User> user = res.getBody().jsonPath().getList("");
+//		System.out.println("xxx ::  " + user);
 
+	}
+
+	@Test
+	public void testGetUserById() {
+		given()
+			.pathParam("id", 1)
+		.when()
+			.get(API_BASE + "/users/{id}")
+		.then()
+			.statusCode(200)
+			.contentType(ContentType.JSON)
+			.body("id", equalTo(1))
+			.body("firstName", equalTo("Jay1"));
+	}
+	
+	@Test
+	public void testUpdateUserById() {
+		// fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testDeleteUserById() {
+		// fail("Not yet implemented");
 	}
 
 }
