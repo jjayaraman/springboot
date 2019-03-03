1b27471f6b5a24693338350f45cde1129fea4d3a
Jayakumar Jayaraman <india.jai@gmail.com>
2019-03-03 20:20:06 +0000

    crud postgre added

---
+++ crud_postgres/src/main/java/com/jai/springboot/crud/User.java
@@ -0,0 +1,39 @@
+package com.jai.springboot.crud;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
