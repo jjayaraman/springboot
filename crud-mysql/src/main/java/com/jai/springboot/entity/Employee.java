683015b11b5e80fc219b3d795d06f83fdbcbf212
jjayaraman <india.jai@gmail.com>
2019-07-14 11:34:46 +0100

    crud mysql

--- crud-mysql/src/main/java/com/jai/springboot/entity/Employee.java
+++ crud-mysql/src/main/java/com/jai/springboot/entity/Employee.java
@@ -4,8 +4,26 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "employees")
+@Table(schema = "employees", name = "employees")
 public class Employee {
 
 
+
 }
+
+
+/*
+
+    CREATE TABLE `employees` (
+        `emp_no` int(11) NOT NULL,
+        `birth_date` date NOT NULL,
+        `first_name` varchar(14) NOT NULL,
+        `last_name` varchar(16) NOT NULL,
+        `gender` enum('M','F') NOT NULL,
+        `hire_date` date NOT NULL,
+        PRIMARY KEY (`emp_no`)
+        );
+        ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+*/
+
