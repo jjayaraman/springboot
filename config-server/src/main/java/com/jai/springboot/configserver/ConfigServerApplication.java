cf749408cacbe121333c0f48677bb8058ae433e9
jjayaraman <india.jai@gmail.com>
2020-04-04 16:40:02 +0100

    updated

--- config-server/src/main/java/com/jai/springboot/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/jai/springboot/configserver/ConfigServerApplication.java
@@ -4,6 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ *
+ * You can access the configurations using URL :
+ * http://localhost:8888/config/dev
+ * http://localhost:8888/config/test
+ * http://localhost:8888/config/prod
+ *
+ * http://localhost:8888/config/dev/master
+ * http://localhost:8888/config/test/master
+ * http://localhost:8888/config/prod/master
+ *
+ * http://localhost:8888/config/dev/feature1
+ * http://localhost:8888/config/test/feature1
+ * http://localhost:8888/config/prod/feature1
+ *
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
